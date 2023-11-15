1f8b273f1977b04d1c42cbfd87c017fcfa99421f
Nguyễn Trung Nghĩa <87295854+trngbro@users.noreply.github.com>
2023-11-15 15:36:56 +0700

    fn warehouse

---
+++ Warehouse/src/main/java/qlk/Controller/WarehouseController.java
@@ -0,0 +1,59 @@
+package qlk.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import qlk.Model.Kho;
+import qlk.Service.KhoService;
+
+@Controller
+public class WarehouseController {
+
+    @Autowired
+    private KhoService khoService;
+
+    @GetMapping("/warehouse")
+    public String getAllKho(Model model) {
+        model.addAttribute("listKho", khoService.listAll());
+        return "warehouseTable";
+    }
+
+    @GetMapping("/warehouse/{id}")
+    public String detailWarehouse(@PathVariable(name = "id") int id, Model model) {
+        model.addAttribute("warehouse", khoService.get(id));
+        return "warehouseDetails";
+    }
+
+    @GetMapping("/warehouse/add")
+    public String showAddForm(Model model) {
+        model.addAttribute("kho", new Kho());
+        return "warehouseForm";
+    }
+
+    @PostMapping("/save_warehouse")
+    public String addKho(@ModelAttribute("kho") Kho kho) {
+        khoService.save(kho);
+        return "redirect:/warehouse/"; 
+    }
+
+    @PostMapping("/warehouse/edit/{id}")
+    public String editKho(@ModelAttribute("kho") Kho kho) {
+        Kho kho_temp = khoService.get(kho.getMakho());
+        kho_temp.setDiachi(kho.getDiachi());
+        kho_temp.setTenkho(kho.getTenkho());
+        khoService.save(kho_temp);
+        return "redirect:/warehouse/";
+    }
+
+    @GetMapping("/warehouse/delete/{id}")
+    public String deleteKho(@PathVariable("id") int id) {
+        try {
+            khoService.delete(id);
+            return "redirect:/warehouse/";
+        } catch (Exception e) {
+            // catch foreign key
+            return "";
+        }
+    }
+}
